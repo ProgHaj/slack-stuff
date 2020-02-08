daf8dbeb8f3d6ecce1c90f4e817f2ba05ffec2d8
Magnus Tullberg <flummeltrollet@gmail.com>
2020-02-08 13:13:57 +0100

    update to not use slack id, gets ending of a slack url instead of whole url.

--- create_thread_url.py
+++ create_thread_url.py
@@ -1,26 +1,20 @@
 # input: url to archived message and the id of the used slack
-# outputs: url to thread of archived message which can be viewed
+# outputs: thread-url to append to any slack url of the slack with the archived message
 
 # example of a slack url with a thread open:
 #    https://app.slack.com/client/SLACK_ID/CHANNEL_ID/thread/CHANNEL_ID-MESSAGE_ID_FIRST_10.MESSAGE_ID_REST
 # example of archived slack url:
 #    https://SLACK_NAME.slack.com/archives/CHANNEL_ID/pMESSAGE_ID
 
-# usage: python create_thread_url.py URL_TO_ARCHIVED_MESSAGE SLACK_ID
+# usage: python create_thread_url.py URL_TO_ARCHIVED_MESSAGE
 
+import sys
 
-def extract(url, slack_id):
+def extract(url, slack_id=None):
     channel, message = url.split("/")[-2:]
-    transformed = "{}-{}.{}".format(channel, message[1:11], message[11:])
-    base = ["https://app.slack.com/client",
-            slack_id,
-            channel,
-            "thread",
-            transformed]
-    new_url = "/".join(base)
-    return new_url
+    return "/thread/{}-{}.{}".format(channel, message[1:11], message[11:])
+
 
 if __name__ == "__main__":
-    import sys
-    url = extract(sys.argv[1], sys.argv[2])
+    url = extract(sys.argv[1])
     print(url)
